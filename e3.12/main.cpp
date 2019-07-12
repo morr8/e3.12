94aae2be83143420c6c2bde9287f4a116542b85c
Sergio Campos <morrodogg8@gmail.com>
2019-07-11 17:35:12 -0700

    copy and pasted from e3.11 and added cin.fail() code

--- e3.12/main.cpp
+++ e3.12/main.cpp
@@ -1,15 +1,69 @@
-//
-//  main.cpp
-//  e3.12
-//
-//  Created by Sergio campos on 7/11/19.
-//  Copyright © 2019 Sergio campos. All rights reserved.
-//
+/*
+ • E3.12
+ Add error handling to Exercise • E3.11. If the user does not enter a number when expected, or provides an invalid unit for the altitude, print an error message and end the program.
+ */
+
 
 #include <iostream>
+#include <string>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std;
+
+int main()
+{
+    cout << "enter temperature in celsius or fahrenheit (example 12 c): ";
+    float temp;
+    string tempSuffix;
+    cin >> temp >> tempSuffix;
+    cout << "enter altitude in meters or feet (example 1,000 ft or 300 m): ";
+    float alt;
+    string altSuffix;
+    cin >> alt >> altSuffix;
+    
+    if (cin.fail())
+    {
+        cout << "\nerror please follow examples" << endl;
+        return 1;
+    }
+    
+    if (altSuffix == "ft")
+    {
+        temp = temp + (alt / 1000);
+    }
+    else
+    {
+        temp = temp + (alt / 300);
+    }
+    
+    if (tempSuffix == "f")
+    {
+        if (temp <= 32)
+        {
+            cout << "solid";
+        }
+        else if (temp < 212)
+        {
+            cout << "liquid";
+        }
+        else
+        {
+            cout << "gaseous";
+        }
+    }
+    else
+    {
+        if (temp <= 0)
+        {
+            cout << "solid";
+        }
+        else if ( temp < 100)
+        {
+            cout << "liquid";
+        }
+        else
+        {
+            cout << "gaseous";
+        }
+    }
+    cout << endl;
 }
